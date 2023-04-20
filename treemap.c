c179649cbfecc7e89075a29c0a08c7409277c67e
Replit user <>
2023-04-20 19:00:24 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -61,7 +61,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
       { //HACIA DERECHA
         if( !tree->current->right){
           
-          tree->current->right==createTreeNode(key,value);
+          tree->current->right=createTreeNode(key,value);
           tree->current->right->parent=tree->current;
           tree->current=tree->current->right;
           return;
